f3b8d83ec99fcd07c2a6189fa44b35c09cb95b23
Murilo Santos Telles <murilosantos.work@gmail.com>
2025-08-30 14:53:37 -0300

    feat: Resolução Exercício 5

---
+++ src/exercicio5/exercicio5.java
@@ -0,0 +1,19 @@
+package exercicio5;
+
+import java.util.Scanner;
+
+public class exercicio5 {
+    public static void main(String[] args) {
+        Scanner leitor = new Scanner(System.in);
+
+
+        System.out.println("Salário mínimo: R$1518,00");
+        System.out.println("Digite o salário a ser recebido: ");
+        double salario = leitor.nextDouble();
+        double salarioMinimo = 1518;
+        double qtdSalariosMinimos = salario/salarioMinimo;
+
+        System.out.println("Seu salário: R$"+salario);
+        System.out.println("Quantidade de salários mínimos: "+qtdSalariosMinimos);
+    }
+}
