11606de11eed29c53147b965d4f355c370ec3130
Murilo Santos Telles <murilosantos.work@gmail.com>
2025-08-31 01:36:19 -0300

    feat: Resolução Exercício 18

---
+++ src/Exercicio18.java
@@ -0,0 +1,17 @@
+public class Exercicio18 {
+    public static void main(String[] args) {
+        double alturaFrancisco = 150;
+        double alturaSara = 110;
+
+        double crescimentoFrancisco = 2;
+        double crescimentoSara = 3;
+        int contadorAnual = 0;
+
+        while (alturaFrancisco >= alturaSara){
+            alturaFrancisco += crescimentoFrancisco;
+            alturaSara += crescimentoSara;
+            contadorAnual += 1;
+        }
+        System.out.println("Sara seria maior que Francisco em "+contadorAnual+" anos.");
+    }
+}
