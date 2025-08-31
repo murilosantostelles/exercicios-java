a89cb416768a56be19f5d3fbebb12b6e7586eb28
Murilo Santos Telles <murilosantos.work@gmail.com>
2025-08-31 00:16:57 -0300

    feat: Resolução Exercício 9

---
+++ src/Exercicio10/Exercicio10.java
@@ -0,0 +1,7 @@
+package Exercicio10;
+
+public class Exercicio10 {
+    public static void main(String[] args) {
+        
+    }
+}
--- src/Exercicio9/Exercicio9.java
+++ src/Exercicio9/Exercicio9.java
@@ -1,7 +1,35 @@
 package Exercicio9;
 
+import java.util.Scanner;
+
 public class Exercicio9 {
     public static void main(String[] args) {
-        
+        Scanner leitor = new Scanner(System.in);
+
+        System.out.println("Insira seu PESO (ex.: 65,5): ");
+        double peso = leitor.nextDouble();
+
+        System.out.println("Insira sua ALTURA (ex.: 1,70): ");
+        double altura = leitor.nextDouble();
+
+        double imc = peso / (altura * altura); // função para calcular potência
+
+        System.out.println("Seu IMC é: "+imc);
+
+        if (imc <= 18.5){
+            System.out.println("Abaixo do peso");
+        } else if (imc >= 18.6 && imc <= 24.9) {
+            System.out.println("Peso ideal");
+        } else if (imc >= 25.0 && imc <= 29.9) {
+            System.out.println("Levemente acima do peso");
+        } else if (imc >= 30 && imc <= 34.9) {
+            System.out.println("Obesidade grau I");
+        } else if (imc >= 35.0 && imc <= 39.9) {
+            System.out.println("Obesidade grau II (severa)");
+        } else if (imc >= 40) {
+            System.out.println("Obesidade grau III (mórbida)");
+        }
+
+
     }
 }
