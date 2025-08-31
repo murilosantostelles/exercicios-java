4ccad5942b68a0b595a32f727ceab3cbeec9f83a
Murilo Santos Telles <murilosantos.work@gmail.com>
2025-08-31 00:21:36 -0300

    feat: Resolução Exercício 10

--- src/Exercicio10/Exercicio10.java
+++ src/Exercicio10/Exercicio10.java
@@ -1,7 +1,24 @@
 package Exercicio10;
 
+import java.util.Scanner;
+
 public class Exercicio10 {
     public static void main(String[] args) {
-        
+        Scanner leitor = new Scanner(System.in);
+
+
+        System.out.println("Primeira nota: ");
+        double nota1 = leitor.nextDouble();
+        System.out.println("Segunda nota: ");
+        double nota2 = leitor.nextDouble();
+        System.out.println("Terceira nota: ");
+        double nota3 = leitor.nextDouble();
+
+        double media = (nota1+nota2+nota3)/3;
+
+        System.out.println("Primeira pontuação: "+nota1);
+        System.out.println("Segunda pontuação: "+nota2);
+        System.out.println("Terceira pontuação: "+nota3);
+        System.out.println("Média: "+media);
     }
 }
